57d4d14aabbbd50ad53858e51d309f245967a7d6
Surya Avala <suryaavala.dev@gmail.com>
2017-05-19 02:17:00 +1000

    reduced the dataload time from 3.xmin to 3sec

--- Project/proj_spec/test.py
+++ Project/proj_spec/test.py
@@ -1,13 +1,17 @@
-import pandas as pd
-from helper import *
 import time
-
-
-
 localtime = time.localtime()
 mi = localtime.tm_min
 sec = localtime.tm_sec
 print('{}:{}'.format(mi, sec))
+
+
+import pandas as pd
+from helper import *
+
+
+
+
+
 lines = read_data('./asset/training_data.txt')
 for i in range(len(lines)):
     lines[i] = lines[i].split(":")
@@ -16,10 +20,7 @@ df = pd.DataFrame()
 labels = ['word' , 'syl']
 df = pd.DataFrame.from_records(lines, columns=labels)
 df['syla'] = df['syl'].str.split(" ")
-# for l in lines:
-#     word, syl = l.split(":")
-#     syl = syl.split(" ")
-#     df = df.append({'word' : word, 'syl' : syl}, ignore_index = True)
+#empty values in a dataframe np.where(pd.isnull(df))
 
 localtime = time.localtime()
 mi = localtime.tm_min

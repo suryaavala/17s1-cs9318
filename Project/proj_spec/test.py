7410cfd41cd6b1bd675699665868931b88602338
Surya Avala <suryaavala.dev@gmail.com>
2017-05-19 13:39:08 +1000

    added pos_tags

--- Project/proj_spec/test.py
+++ Project/proj_spec/test.py
@@ -7,8 +7,8 @@ print('{}:{}'.format(mi, sec))
 
 import pandas as pd
 from helper import *
-
-
+import nltk
+#print(nltk.pos_tag(['Surya']))
 
 
 
@@ -20,6 +20,8 @@ df = pd.DataFrame()
 labels = ['word' , 'syl']
 df = pd.DataFrame.from_records(lines, columns=labels)
 df['syla'] = df['syl'].str.split(" ")
+#df['type'] = df['word'].nltk.pos_tag()
+df['type'] = df['word'].apply(nltk.pos_tag)
 #empty values in a dataframe np.where(pd.isnull(df))
 
 localtime = time.localtime()

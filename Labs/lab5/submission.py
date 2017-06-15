fdfa84a5f3330d189af213d670479c65d6c60a28
Surya Avala <suryaavala.dev@gmail.com>
2017-06-15 18:26:50 +1000

    lab5 added

---
+++ Labs/lab5/submission.py
@@ -0,0 +1,6 @@
+## import modules here 
+
+################# Question 1 #################
+
+def lf(R, Q, P, max_iter, _err, _mu_1, _mu_2, _lambda_1, _lambda_2):# do not change the heading of the function
+    pass # **replace** this line with your code
